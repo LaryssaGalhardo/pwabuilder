e7497c0ae240bf200b7e6a2820a77c5584f67755
LaryssaGalhardo <65986329+LaryssaGalhardo@users.noreply.github.com>
2020-10-22 13:22:27 -0300

    Add files via upload

---
+++ tarefa/contato.php
@@ -0,0 +1,94 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fofocas</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/fofo.css">
+</head>
+<body>
+    
+    <div class="jumbotron jumbotron-fluid jumbotron-fofocas">
+        <h1 class="display-4 text-center text-black">FOFOCAS ON-LINE</h1>
+        <p class="lead text-center text-black">Quer saber o que acontece no mundo dos famosos?</p>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success negative-top">
+        <a class="navbar-brand text-white">FOFOCAS</a>
+        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="my-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="contato.php">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+     <div class="container mt-2">
+         <div class="row">
+             <div class="col-12 col-sm-6 col-md-6">
+                 <img class="img-fluid mt-5" src="img/fale.png" alt="ouvidoria">
+             </div>
+             <div class="col-12 col-sm-6 col-md-6">
+                 <form class="mt-3" action="">
+                     <div class="form-group">
+                         <label>Nome Completo</label>
+                         <input type="text" name="" id="" class="form-control">
+                     </div>
+                     <div class="form-group">
+                         <label>E-mail</label>
+                         <input type="email" name="" id="" class="form-control">
+                   </div>
+                <div class="form-group row">
+                    <div class="col-12 col-sm-6 col-md-6">
+                        <label>Celular</label>
+                        <input type="tel" name="" id="" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Assunto</label>
+                    <select name="" id="" class="form-control">
+                        <option value="">O assunto é...</option>
+                        <option value="">Sugestões</option>
+                        <option value="">Criticas</option>
+                        <option value="">Outros</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Mensagem</label>
+                    <textarea name="" id="" cols="15" rows="5" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <button class="btn-success">Enviar</button>
+                </div>
+
+                 </form>
+             </div>
+         </div>
+     </div>
+    
+    
+
+    <div class="row mt-5 bg-success text-dark">
+        <div class="col-12 text-center mt-3 mb-3">
+                <span class="copyrigtht">Todos direitos reservardos:
+                <br>Laryssa Bueno Galhardo</span>
+            </div>
+           </div>
+        
+        
+
+
+
+
+                
+
+    <script src="js/jquery-3.4.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
